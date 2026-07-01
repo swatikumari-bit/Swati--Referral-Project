8573e590ddee42889f80f1facca68e93d965e4b1
swatikumari-bit <swati.kumari@pickyourtrail.com>
2026-07-02 01:54:44 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzJp_huVJTQtwA9jSRd3vYcz2NlMAMUWTerH8iAuTE9t3zneb0jR_rXFiaiFCHTYrgj/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzp_bWg77LjYzEDkqtdE0uTp5BTgjbcuoRU4nK0XAAx0A-0GAQ0C5n7VAMzNzKvuKTk/exec",
   REFRESH_INTERVAL_MS: 5 * 60 * 1000, // 5 minutes
   COMPANY_NAME: "Pickyourtrail",
   TABLE_PAGE_SIZE: 10
